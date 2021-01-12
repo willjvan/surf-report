60e11c1bc40fc2099f41937c95c125f0d7f50958
William <willjvan@shaw.ca>
2021-01-12 14:02:36 -0800

    further front end dev

--- client/client.js
+++ client/client.js
@@ -1,4 +1,16 @@
-console.log("hello");
+const form = document.getElementsByClassName('search')[0]
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // dont reload
+    const formData = new FormData(form);
+    const surfSpot = formData.get('searchSpot')
+    const mew = {
+        surfSpot
+    }
+    console.log(mew);
+});
+
+document.getElementsByClassName('search')[0].style.display = "none";
 
 function openSlideMenu(){
     document.getElementsByClassName('forecast-container')[0].style.display = "block";
